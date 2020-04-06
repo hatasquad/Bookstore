bacf5cd3f43fd213c5c7d44c8075140c7e1f5038
marodi-mykhailo <mykhailo.marodi@gmail.com>
2020-04-06 02:48:35 +0200

    added plugin Slick slider; Added primary 2 sliders; fix some bugs

---
+++ js/slider.js
@@ -0,0 +1,50 @@
+$('.slider__box').slick({
+    dots: true,
+    infinite: true,
+    speed: 600,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+    autoplay: true,
+    autoplaySpeed: 6000,
+    swipeToSlide: true
+  });
+
+  $('.second-slider').slick({
+    dots: true,
+    infinite: true,
+    speed: 600,
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 6000,
+    arows: true,
+    swipeToSlide: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
