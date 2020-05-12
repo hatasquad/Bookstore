52cc64bc891d71f217ba108b581bbfa82830a327
marodi-mykhailo <mykhailo.marodi@gmail.com>
2020-05-12 21:41:50 +0200

    fixed height, fix some styles

--- js/slider.js
+++ js/slider.js
@@ -15,7 +15,7 @@ $('.slider__box').slick({
     dots: true,
     infinite: true,
     speed: 600,
-    slidesToShow: 5 ,
+    slidesToShow: 4 ,
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 6000,
