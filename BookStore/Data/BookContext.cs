4012f310e18ae4e4d3f5e463c900ee426336d614
Yurii Shchehliuk <shchehliuk124@gmail.com>
2020-05-14 10:19:44 +0200

    initial commit

---
+++ BookStore/Data/BookContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using BookStore.Models;
+
+namespace BookStore.Data
+{
+    public class BookContext : DbContext
+    {
+        DbSet<LoginViewModel> loginViewModels { get; set; }
+       
+    }
+}
