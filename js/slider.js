778550ee8988ab20334162fafbf373d1592f35fa
marodi-mykhailo <mykhailo.marodi@gmail.com>
2020-05-14 11:07:47 +0200

    fix scripts

--- js/slider.js
+++ js/slider.js
@@ -1,4 +1,61 @@
+$(document).ready(function(){
+  $('.slider__box').slick({
+    dots: true,
+    infinite: true,
+    speed: 600,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+    autoplay: true,
+    autoplaySpeed: 6000,
+    swipeToSlide: true,
+    nextArrow: '<i class="fas fa-arrow-circle-right slider-btn slider-next"></i>',
+    prevArrow: '<i class="fas fa-arrow-circle-left slider-btn slider-prev"></i>'
+  });
 
+  $('.second-slider').slick({
+    dots: true,
+    infinite: true,
+    speed: 600,
+    slidesToShow: 4 ,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 6000,
+    
+    swipeToSlide: true,
+    nextArrow: '<i class="fas fa-arrow-circle-right slider-btn slider-next2"></i>',
+    prevArrow: '<i class="fas fa-arrow-circle-left slider-btn slider-prev2"></i>',
+  
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
+
+})
   $('.slider__box').slick({
       dots: true,
       infinite: true,
