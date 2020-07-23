cb27bce3f05570185a3a454e41ffb29c9bf35cd8
marodi-mykhailo <mykhailo.marodi@gmail.com>
2020-07-23 20:00:43 +0200

    Add Admin page

--- admin/function.php
+++ admin/function.php
@@ -9,6 +9,7 @@ function connect(){
     if (!$conn) {
         die("Connection failed: " . mysqli_connect_error());
     }
+    mysqli_set_charset($conn, "utf8");
     return $conn;
 }
 
@@ -16,7 +17,7 @@ function connect(){
 function init(){
     //вывожу список товаров
     $conn = connect();
-    $sql = "SELECT * FROM Books";
+    $sql = "SELECT id, title FROM Books";
     $result = mysqli_query($conn, $sql);
     if (mysqli_num_rows($result) > 0) {
         $out = array();
@@ -29,5 +30,80 @@ function init(){
     }
     mysqli_close($conn);
 }
+function selectOneGoods(){
+    $conn = connect();
+    $id = $_POST['bid'];
+    $sql = "SELECT * FROM Books WHERE id = '$id'";
+    $result = mysqli_query($conn, $sql);
+    if (mysqli_num_rows($result) > 0) {
+        $row = mysqli_fetch_assoc($result);
+        echo json_encode($row);
+    } else {
+        echo "0";
+    }
+    mysqli_close($conn);
+}
+
+function updateGoods(){
+    $conn = connect();
+    $id = $_POST['id'];
+    $title = $_POST['btitle'];
+    $author = $_POST['bauthor'];
+    $price = $_POST['bprice'];
+    $img = $_POST['bimg'];
+    $desc = $_POST['bdesc'];
+    $isNew = $_POST['bisnew'];
+
+
+    $sql = "UPDATE Books SET title ='$title', price = '$price', author = '$author', description = '$desc', img = '$img', isNew = '$isNew' WHERE id = '$id'";
+    if(mysqli_query($conn, $sql)){
+       echo  1;
+    }else{
+        echo "Error updating record" . mysqli_error($conn);
+    }
+
+    mysqli_close($conn);
+    writeJSON();
+}
+
+function newGoods(){
+    $conn = connect();
+    $title = $_POST['btitle'];
+    $author = $_POST['bauthor'];
+    $price = $_POST['bprice'];
+    $img = $_POST['bimg'];
+    $desc = $_POST['bdesc'];
+    $isNew = $_POST['bisnew'];
+
+
+    $sql = "INSERT INTO Books(title,price,author,description,img,isNew) VALUES ('$title', '$price', '$author', '$desc', '$img', '$isNew')";
+    if(mysqli_query($conn, $sql)){
+        echo  1;
+    }else{
+        echo "Error updating record" . mysqli_error($conn);
+    }
+
+    mysqli_close($conn);
+    writeJSON();
+}
+
+function writeJSON(){
+    $conn = connect();
+    $sql = "SELECT * FROM Books";
+    $result = mysqli_query($conn, $sql);
+    if (mysqli_num_rows($result) > 0) {
+        $out = array();
+        while($row = mysqli_fetch_assoc($result)) {
+            $out[$row["title"]] = $row;
+        }
+
+        file_put_contents('../goods.json', json_encode($out));
+    } else {
+        echo "0";
+    }
+    mysqli_close($conn);
+
+}
 
 ?>
+
--- admin/js/admin.js
+++ admin/js/admin.js
@@ -12,14 +12,89 @@ function showGoods(data) {
     data = JSON.parse(data);
     console.log(data);
     var out='<select>';
-    out +='<option data-id="0">Новый товар</option>';
+    out +='<option data-id="0">Add book</option>';
     for (var id in data) {
-        out +=`<option data-id="${id}">${data[id].title}</option>`;
+        out +=`<option data-id="${data[id].id}">${data[id].title}</option>`;
     }
     out +='</select>';
     $('.goods-out').html(out);
+    $('.goods-out select').on('change', selectGoods);
 }
+function selectGoods(){
+let id = $('.goods-out select option:selected').attr('data-id');
+ $.post(
+     "core.php",
+     {
+         "action" : "selectOneGoods",
+         "bid" : id
+     },
+     function (data) {
+        data = JSON.parse(data);
+        $('#btitle').val(data.title);
+        $('#bauthor').val(data.author);
+        $('#bprice').val(data.price);
+        $('#bimg').val(data.img);
+        $('#bdesc').val(data.description);
+        $('#bisnew').val(data.isNew);
+        $('#bid').val(data.id);
+     }
+ );
+}
+
+function saveToDb(){
+    let id = $('#bid').val();
+    if (id!=""){
+        $.post(
+            "core.php",
+            {
+                "action" : "updateGoods",
+                "id": id,
+                "btitle":  $('#btitle').val(),
+                "bauthor":  $('#bauthor').val(),
+                "bprice":  $('#bprice').val(),
+                "bimg":  $('#bimg').val(),
+                "bdesc":  $('#bdesc').val(),
+                "bisnew":  $('#bisnew').val(),
+
+            },
+            function (data) {
+                if (data==1){
+                    alert('Item Updated')
+                    init();
+                }else {
+                    console.log(data);
+                }
+            }
+        )
+    }else {
+        $.post(
+            "core.php",
+            {
+                "action" : "newGoods",
+                "id": 0,
+                "btitle":  $('#btitle').val(),
+                "bauthor":  $('#bauthor').val(),
+                "bprice":  $('#bprice').val(),
+                "bimg":  $('#bimg').val(),
+                "bdesc":  $('#bdesc').val(),
+                "bisnew":  $('#bisnew').val(),
+
+            },
+            function (data) {
+                if (data==1){
+                    alert('Item Updated')
+                    init();
+                }else {
+                    console.log(data);
+                }
+            }
+        )
+    }
+}
+
+
 
 $(document).ready(function () {
    init();
+   $('.addToDb').on('click', saveToDb)
 });
